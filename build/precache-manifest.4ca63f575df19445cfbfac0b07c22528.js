0e0b0c95ca14b244f1e79359ad8f3ea937be900f
ABDALLAHAHMAD <lyl44271@gmail.com>
2020-09-29 07:56:23 +0300

    adding all files

---
+++ build/precache-manifest.4ca63f575df19445cfbfac0b07c22528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e3e334e74af309f346c04ff2c3e9ad",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "db7518389a2d0ca716f6",
+    "url": "/monster-rolodex/static/css/main.d95c37ff.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7518389a2d0ca716f6",
+    "url": "/monster-rolodex/static/js/main.b9344479.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
